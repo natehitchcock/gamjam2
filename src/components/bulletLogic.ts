f86f26cd6107c1b663300119a99a7865cb1976f2
thebatman1047 <soldier.of.dawn15@gmail.com>
2017-05-09 00:53:25 -0700

    more bulletLogic

--- src/components/bulletLogic.ts
+++ src/components/bulletLogic.ts
@@ -23,15 +23,17 @@ export default class BulletLogic implements IComponent{
     }
 
     movement(dt: number) {
-        const direction = new THREE.Vector3();
         const speed = require('./toml/weapon.toml');
         const pattern = require('./toml/weapon.toml');
-
+        const direction = new THREE.Vector3();
         direction.copy(this.weaponReference.position);
-
-
-
-
+        
+        direction.add(
+        if(weapon.fired) direction.add(Mouse.THREE.Vector3(x, y, z) += speed; 
+        if(weapon.fired) direction.y -= speed; 
+        if(weapon.fired) direction.x += speed;
+        if(weapon.fired) direction.x -= speed; 
+Weapon.spawn
    } 
     
     update(dt: number) {
