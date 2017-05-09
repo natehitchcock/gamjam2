23eae66791129f2272bb3dd0c48f8cd5426e7103
thebatman1047 <soldier.of.dawn15@gmail.com>
2017-05-09 00:05:24 -0700

    continuing to develop bulletLogic component

--- src/components/bulletLogic.ts
+++ src/components/bulletLogic.ts
@@ -1,19 +1,40 @@
 import Collider from './collider';
 import Sprite from './sprite';
 import { IComponent } from "./component";
+import Entity from "../entity";
+import * as THREE from "@types/three";
 
 interface IBulletData {
     damageMultiplier: number;
     bulletLife: number;
 }
+
 export default class BulletLogic implements IComponent{
     data: IBulletData; 
+    weaponReference: Entity;
 
     constructor(data: IBulletData, owner: Entity) {
         this.data = data;
-    }
-}
+        this.weaponReference = owner; 
+    };
 
+    disappear(dt: number) {
+
+    }
+
+    movement(dt: number) {
+        const direction = new THREE.Vector3();
+        const speed = require('./toml/weapon.toml');
+        const pattern = require('./toml/weapon.toml');
+
+        direction.copy(this.weaponReference.position);
+
+
+
+
+   } 
+    
     update(dt: number) {
         return;
     }
+}
