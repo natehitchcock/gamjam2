0cbc1e0b64b10f7a1ac11255621aa09365870051
natehitchcock <tripnull@gmail.com>
2017-12-30 13:28:51 -0800

    Added beginning of react HUD

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import * as THREE from 'three';
 import { levelManager } from './level';
-import './interface.tsx';
-import { IHudWindow } from "./interface";
+import './interface';
 import EffectComposer from './lib/postprocessing/EffectComposer';
 import RenderPass from './lib/postprocessing/RenderPass';
 import ShaderPass from './lib/postprocessing/ShaderPass';
@@ -16,34 +15,6 @@ declare const window: IGameWindow;
 const scene = new THREE.Scene();
 
 window.scene = scene;
-
-
-/*////////////////////////////////// Hud
-var hudCanvas = document.createElement('canvas');
-
-hudCanvas.width = innerWidth;
-hudCanvas.height = innerHeight;
-
-var hudBitmap = hudCanvas.getContext('2d');
-hudBitmap.font = "Normal 40px Arial";
-hudBitmap.textAlign = 'center';
-hudBitmap.fillStyle = "rgba(245, 245, 245, 0.75)";
-hudBitmap.fillText('HUD Attempt #1...', innerWidth / 2, innerHeight / 2);
-
-var cameraHUD = new THREE.OrthographicCamera(-innerWidth / 2, innerWidth / 2, innerHeight / 2, -innerHeight / 2, 0, 30)
-
-var sceneHUD = new THREE.Scene();
-
-var hudTexture = new THREE.Texture(hudCanvas)
-hudTexture.needsUpdate = true;
-
-var material = new THREE.MeshBasicMaterial({ map: hudTexture });
-material.transparent = true;
-
-var planeGeometry = new THREE.PlaneGeometry(innerWidth, innerHeight);
-var plane = new THREE.Mesh(planeGeometry, material);
-sceneHUD.add(plane);
-/////////////////*/
 
 const renderer = new THREE.WebGLRenderer({ antialias: false });
 renderer.setSize(window.innerWidth, window.innerHeight);
--- src/interface.tsx
+++ src/interface.tsx
@@ -1,54 +1,31 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-// http://media.tojicode.com/webgl-samples/hud-test.html
-
-class Hud {
-  health: number;
-  ammo: number;
-  clipSize: number;
-  logObj: string[];
-  ba_dings: number;
-
-  renderFallback() {
-    ReactDOM.render(
-      <div>
-        <div id="health">
-          <b>Ba-dings:</b> {hwnd.hud.ba_dings}
-        </div>
-      </div>,
-      document.getElementById('hud')
-    );
-  }
-
-  render() {
-    ReactDOM.render(
-      <div>
-        <div id="health">
-          <b>HP:</b> {hwnd.hud.health}
-        </div>
-        <div id="ammo">
-          <b>Ammo:</b> {hwnd.hud.ammo}/{hwnd.hud.clipSize}
-        </div>
-        <div id="log">{
-          hwnd.hud.logObj.forEach((element) => {
-            <div>{element}</div>
-          })}
-        </div>
-      </div>,
-      document.getElementById('hud')
-    );
-  }
+class HUD extends React.Component<any, any> {
+    render() {
+        return <h1> This is the HUD </h1>;
+    }
 }
 
-export interface IHudWindow extends Window {
-  hud: Hud;
+interface IAppState {
+    HUDActive: boolean;
 }
 
-let hwnd = window as IHudWindow;
-hwnd.hud = new Hud();
-hwnd.hud.health = 7;
-hwnd.hud.ammo = 50;
-hwnd.hud.clipSize = 100;
-hwnd.hud.logObj = ["abc", "def"];
-hwnd.hud.ba_dings = 0;
+class App extends React.Component<any, IAppState> {
+    constructor(props) {
+        super(props);
+        this.state =  {
+            HUDActive: true,
+        };
+    }
+
+    render() {
+        return this.state.HUDActive ? <HUD/> : <div/>;
+    }
+}
+
+const container = document.createElement('div');
+container.setAttribute("id", "app-wrapper");
+document.body.appendChild(container);
+
+ReactDOM.render(<App/>, container);
